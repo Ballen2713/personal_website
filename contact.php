53d68cc9a44e51f76120c09da6ef8700ca23ff0a
Ballen2713 <ballen15@students.towson.edu>
2020-06-07 00:34:49 -0400

    Updated php contact form code

--- contact.php
+++ contact.php
@@ -4,11 +4,16 @@ if(isset($_POST['submit'])) {
     $from = $_POST['email'];
     $full_name = $_POST['full_name'];
     $subject = $_POST['subject'];
+    $subject2 = "Copy of your form submission";
     $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
+    $headers .="MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8";
     $headers2 = "From:" . $to;
     mail($to, $subject, $message, $headers);
-    echo "Mail Sent. Thank you"
+    mail($from, $subject2, $message2, $headers2);
+    echo "Mail Sent. Thank you";
 }
 ?>
