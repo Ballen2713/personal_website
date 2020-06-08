62cb998e096a1af071b8a8c9c3813f3f61af0a42
Ballen2713 <ballen15@students.towson.edu>
2020-06-07 22:49:08 -0400

    Added php file for sending email from filled out form

--- contact.php
+++ contact.php
@@ -1,24 +1,49 @@
 <?php
 if(isset($_POST['submit'])) { 
-    $to = "bart.allen27@outlook.com";
-    $from = $_POST['email'];
-    $full_name = $_POST['full_name'];
+    $mailTo = "ballen7294@gmail.com";
+    $mailFrom = $_POST['email'];
+    $full_name = $_POST['name'];
     $subject = $_POST['subject'];
-    $subject2 = "Copy of your form submission";
-    $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
+    $message = $_POST['message'];
+    $headers = "From: " .$mailFrom;
+    $txt = "You have recieved an e-mail from " .$full_name.".\n\n" .$message;
+    //header("Location: index.html");
 
-    $headers = "From:" . $from;
-    // $headers .="MIME-Version: 1.0\r\n";
-    // $headers .= "Content-type: text/html; charset=utf-8";
-    $headers2 = "From:" . $to;
-    if(mail($to, $subject, $message, $headers)){ 
-        echo "<h1>Sent Succesfully! Thank You". " ".$full_name.", I shall respond shortly</h1>";
-    }else { 
-        echo "Something went wrong 😥";
+
+    //Error Handling
+    $errorEmpty = false;
+    $errorEmail = false;
+
+    if(empty($full_name)|| empty($mailFrom) || empty($message)){ 
+        echo "<span class='form-error'> Fill in all fields! </span>";
+        $errorEmpty = true;
     }
-    
-    // mail($from, $subject2, $message2, $headers2);
-    // echo "Mail Sent. Thank you";
+    elseif(!filter_var($mailFrom, FILTER_VALIDATE_EMAIL)) { 
+        echo "<span class='form-error'>Write a valid e-mail address! </span>";
+        $errorEmail = true;
+    }else { 
+        mail($mailTo, $subject, $txt, $headers);
+        echo "<span class='form-success'> Email Sent sucessfully! </span>";
+    }
+}
+else { 
+    echo "There was an error sending an e-mail";
 }
 ?>
+
+<script>
+    $("#mail-full_name, #mail-email_address, #mail-subject, #mail-msg").removeClass("input-error");
+    var errorEmpty = "<?php echo $errorEmpty;?>";
+    var errorEmail = "<?php echo $errorEmail;?>";
+
+    if(errorEmpty == true) { 
+        $("#mail-full_name, #mail-email_address, #mail-subject, #mail-msg").addClass("input-error")
+        console.log("No fields filled out");
+    }
+    if(errorEmail == true) { 
+        $("#mail-email_address").addClass("input-error");
+    }
+    if (errorEmpty == false & errorEmail == false) { 
+        $("#mail-full_name, #mail-email_address, #mail-subject, #mail-msg").val("");
+    }
+</script>
